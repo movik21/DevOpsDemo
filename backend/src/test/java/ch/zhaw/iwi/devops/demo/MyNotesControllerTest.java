74954a0d38d3b026b2e2614f8a0a0efb2e7549f4
movik21 <moorvik1@students.zhaw.ch>
2024-04-29 23:45:43 +0200

    Added test case delecte note

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/MyNotesControllerTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/MyNotesControllerTest.java
@@ -1,6 +1,7 @@
 package ch.zhaw.iwi.devops.demo;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +14,16 @@ public class MyNotesControllerTest {
         controller.createMyNote(1, mynote);
         assertEquals(1, controller.count());
         assertEquals(1, controller.mynote().size());
+    }
+    @Test
+    public void testDelete() {
+        MyNotesController controller = new MyNotesController();
+        var mynote = new MyNotes(1, "title", "description");
+        controller.createMyNote(1, mynote);
+
+        controller.deleteMyNote(1);
+        assertEquals(0, controller.count()); 
+        assertTrue(controller.mynote().isEmpty());
     }
 
 }
